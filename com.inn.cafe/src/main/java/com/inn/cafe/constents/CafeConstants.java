6387bd0b40a792c41e72acc3f4ce6fc6bfaa41eb
Abeed Mantoo <mantooabeed@gmail.com>
2025-07-08 01:01:40 +0530

    ✅ Fixed user table issue by renaming entity to 'users'

--- com.inn.cafe/src/main/java/com/inn/cafe/constents/CafeConstants.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/constents/CafeConstants.java
@@ -1,5 +1,12 @@
 package com.inn.cafe.constents;
 
 public class CafeConstants {
+	
+	public static final String SOMETHING_WENT_WRONG="Something Went Wrong.";
+
+	public static final String INVALID_DATA="Invalid Data.";
+	
+	public static final String NRE_DATA="New Data.";
+
 
 }
--- com.inn.cafe/src/main/java/com/inn/cafe/dao/UserDao.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/dao/UserDao.java
@@ -1,5 +1,11 @@
 package com.inn.cafe.dao;
 
-public interface UserDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
+import com.inn.cafe.POJO.User;
+
+public interface UserDao extends JpaRepository<User,Integer>{
+
+	User findByEmailId(@Param("email") String email);
 }
