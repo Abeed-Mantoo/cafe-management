441f969c626b7d1872e10ee6a58834edfa01043b
Abeed Mantoo <mantooabeed@gmail.com>
2025-07-25 23:55:03 +0530

    Cleaned up tracked files using .gitignore

---
+++ com.inn.cafe/src/main/java/com/inn/cafe/JWT/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.inn.cafe.JWT;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig implements WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure configure(AuthenticationManagerBuilder auth) throws Exception{
+		super.configure(auth);
+	}
+	
+	protected void new{
+		super.configure(auth);
+	}
+	
+}
